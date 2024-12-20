e4b691ad61a371b7e2dcc2eb493e48be7608f23a
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-20 11:41:38 -0500

    Setea la base de los test de fetch

--- src/tsx/hooks/__tests__/useFetch.test.tsx
+++ src/tsx/hooks/__tests__/useFetch.test.tsx
@@ -1,10 +1,14 @@
-import { describe, it, vi, expect } from 'vitest';
+import { describe, it, vi, expect, expectTypeOf } from 'vitest';
 import { renderHook } from '@testing-library/react';
-import { makeFetchUrl } from '../useFetch.tsx';
+import { makeFetchUrl, useFetch } from '../useFetch.tsx';
+
+const mockFetch = vi.fn();
+globalThis.fetch = mockFetch;
 
 describe('makeFetchUrl:', () => {
     it('returns a string', () => {
         const data = makeFetchUrl();
+
         expect(typeof data === 'string').toBe(true);
     });
 
@@ -15,9 +19,48 @@ describe('makeFetchUrl:', () => {
 
         expect(argumentRegex.test(data)).toBe(true);
     });
-    it('returns a url without any category defined, at the end of the string, if no argument is passed', () => {
+    it('returns a url with category at the end of the string if no argument is passed', () => {
         const data = makeFetchUrl();
 
         expect(/category$/.test(data));
+    });
+});
+
+describe('useFetch:', () => {
+    const mockedItemData = {
+        id: '1',
+        title: 'Sony WH-1000XM3 Bluetooth Wireless Over Ear Headphones with Mic (Silver)',
+        image: 'https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692947383286-714WUJlhbLS._SL1500_.jpg',
+        price: 773,
+        description: 'Digital noise cancelling : Industry leading Active',
+        brand: 'Sony',
+        Color: 'silver',
+        Category: 'audio',
+        discount: 0,
+    };
+
+    const mockedCategoriesData = [
+        'electronics',
+        'jewelery',
+        "men's clothing",
+        "women's clothing",
+    ];
+
+    it('Takes a string and returns an object with onLoad, onError and data keys', () => {
+        mockFetch.mockResolvedValueOnce({
+            ok: true,
+            json: async () => mockedItemData,
+        });
+
+        const data = useFetch(makeFetchUrl());
+        const fetchResponse = ['data', 'onLoad', 'onError'];
+
+        for (const key of fetchResponse) {
+            expect(key in data).toBe(true);
+        }
+    });
+
+    it('sets the value of onLoad from false to true once the fetch is done', () => {
+        //
     });
 });
--- src/tsx/hooks/useFetch.tsx
+++ src/tsx/hooks/useFetch.tsx
@@ -1,7 +1,41 @@
-const makeFetchUrl = (category: string = ''): string => {
-    const providerUrl = 'https://fakestoreapi.in/api/products';
-    const categoryName = category ? `/category?type=${category}` : '/category';
-    return `${providerUrl}${categoryName}`;
+type CategoryPath = '/category' | `/category?type=${string}`;
+type ApiUrl = `${typeof baseUrl}${CategoryPath}`;
+type ProductCategories = string[];
+
+interface ProductData {
+    id: number;
+    title: string;
+    model: string;
+    image: string;
+    price: number;
+    brand: string;
+    onSale: boolean;
+    description: string;
+}
+
+type ErrorState = [boolean, string] | null;
+
+// interface FetchError {
+//     code: number | 'unknown';
+//     description: string[];
+// }
+
+type FetchReturn = {
+    onLoad: boolean;
+    data: DataFetched | null;
+    onError: ErrorState | null;
 };
 
-export { makeFetchUrl };
+type DataFetched = ProductData[] | ProductCategories;
+
+const baseUrl = 'https://fakestoreapi.in/api/products' as const;
+const makeFetchUrl = (category: string = ''): ApiUrl => {
+    const categoryName = category ? `/category?type=${category}` : '/category';
+    return `${baseUrl}${categoryName as CategoryPath}`;
+};
+
+const useFetch = (url: ApiUrl): FetchReturn => {
+    return { data: null, onError: null, onLoad: false };
+};
+
+export { makeFetchUrl, useFetch };

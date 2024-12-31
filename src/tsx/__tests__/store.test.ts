222700000bd1ddd29bbb5fbb97b457ba24ae1ad9
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-31 15:44:52 -0500

    Crea el archivo de mock y la base de testeo para store

---
+++ src/tsx/__tests__/store.test.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { Store } from '../store';
+import { mockedCategoriesData, mockedItemsData } from './mockData';
+import { fetcher, makeFetchUrl } from '../fetcher';
+
+const mockFetch = vi.fn();
+globalThis.fetch = mockFetch;
+
+beforeEach(() => {
+    mockFetch.mockReset();
+    mockFetch.mockImplementation(async (url: string) => {
+        if (url.includes('?type') || /[0-9]$/.test(url)) {
+            const match = url.match(/[0-9]+/);
+            const amount = match ? +match[0] : mockedItemsData.length;
+            return {
+                ok: true,
+                json: async () => mockedItemsData.slice(0, amount),
+            };
+        } else if (/category$/.test(url)) {
+            return {
+                ok: true,
+                json: async () => mockedCategoriesData,
+            };
+        }
+    });
+});
+
+describe('Store as a singleton', () => {
+    it('returns the same instace from multiple calls', () => {
+        const instance1 = Store.create();
+        const instance2 = Store.create();
+
+        expect(instance1).toStrictEqual(instance2);
+    });
+    it('populate the categories qhen created', async () => {
+        const store = await Store.create();
+
+        expect(store.allCategories).toEqual(mockedCategoriesData);
+    });
+});

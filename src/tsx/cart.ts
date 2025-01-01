4ea493815e6a3ca039e691ec51ffce70fc2a199e
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-01 13:42:03 -0500

    Asegura código

--- src/tsx/cart.ts
+++ src/tsx/cart.ts
@@ -3,23 +3,31 @@ import { ProductData } from './types/global';
 export class Cart {
     private quantities: Record<string, number> = {};
     private static instance: Cart | null = null;
+    private inventory: ProductData[] = [];
 
-    constructor() {
+    constructor(inventory: ProductData[]) {
         if (Cart.instance) {
             return Cart.instance;
         }
+        this.inventory = inventory;
     }
 
     addItem(id: string): void {
+        if (!this.inventory.some((item) => item.id === id)) {
+            throw new Error(`Item with id: ${id} does not exist in inventory`);
+        }
         this.quantities[id] = (this.quantities[id] || 0) + 1;
     }
 
     setAmount(id: string, quantity: number): void {
+        if (!this.inventory.some((item) => item.id === id)) {
+            throw new Error(`Item with id: ${id} does not exist in inventory`);
+        }
         this.quantities[id] = quantity;
     }
 
-    getAmount(id: string): number {
-        return this.quantities[id] ?? 0;
+    getAmount(id: string): number | null {
+        return this.quantities[id] ?? null;
     }
 
     subItem(id: string): void {
@@ -27,10 +35,15 @@ export class Cart {
             return;
         }
         this.quantities[id] -= 1;
+
+        if (this.quantities[id] === 0) {
+            console.log(this.quantities[id]);
+            this.deleteItem(id);
+        }
     }
 
     deleteItem(id: string): void {
-        if (!this.quantities[id]) {
+        if (this.quantities[id] === undefined) {
             return;
         }
         delete this.quantities[id];
@@ -44,16 +57,17 @@ export class Cart {
         this.quantities = {};
     }
 
-    itemTotalCost(id: string, inventory: ProductData[]): number {
-        const itemPrice = inventory.find((item) => item.id === id)?.price ?? 0;
+    itemTotalCost(id: string): number {
+        const itemPrice =
+            this.inventory.find((item) => item.id === id)?.price ?? 0;
         const itemAmount = this.quantities[id] ?? 0;
         return itemAmount * itemPrice;
     }
 
-    totalCost(inventory: ProductData[]): number {
+    totalCost(): number {
         let cost = 0;
         for (const itemId of Object.keys(this.quantities)) {
-            cost += this.itemTotalCost(itemId, inventory);
+            cost += this.itemTotalCost(itemId);
         }
         return cost;
     }

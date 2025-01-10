9e7d1be18ab4de21c4b85153bdf562ac423fef8a
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-10 17:24:55 -0500

    Incorpora reactRouter y crea la ruta base

--- src/tsx/main.tsx
+++ src/tsx/main.tsx
@@ -1,8 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { App } from './app';
+
+const route = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <h1>123</h1>
+        <RouterProvider router={route} />
     </StrictMode>,
 );

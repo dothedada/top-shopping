4d74901581d417eace624dffd5c080319ee9a856
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-11 15:36:30 -0500

    Adds error page

---
+++ src/tsx/routes/errorPage.tsx
@@ -0,0 +1,14 @@
+import { useRouteError } from 'react-router-dom';
+import { ErrorReactRouter } from '../types/global';
+export default function ErrorPage() {
+    const error = useRouteError() as ErrorReactRouter;
+
+    return (
+        <>
+            <h1>OOOPPPPSSSSS!!!</h1>
+            <p>Algo sucedio y la información que buscas no está disponible</p>
+            <h2>{error.statusText}</h2>
+            <p>{error.message}</p>
+        </>
+    );
+}

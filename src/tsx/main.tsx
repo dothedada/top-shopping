bc726a2248cb1c1b09f5bb027baa300823b930a5
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-12 11:37:12 -0500

    Add the fetch for initial items and categories

--- src/tsx/main.tsx
+++ src/tsx/main.tsx
@@ -6,10 +6,88 @@ import ErrorPage from './routes/errorPage';
 import CategoryPage, { categoryLoader } from './routes/categoryPage';
 import ProductDetail from './routes/productDetail';
 import { Store } from './store';
+import { fetcher, makeFetchUrl } from './dataFetcher';
+import { ProductCategories, ProductData } from './types/global';
 
 async function storeLoader() {
   const store = await Store.create();
+  console.log('asd', store.allItems);
   return { store };
+}
+
+// private async populateStore() {
+//   try {
+//     const controller = new AbortController();
+//     const { data, onError } = await fetcher(makeFetchUrl('', 5), controller);
+//     if (onError?.[0]) {
+//       throw onError[1];
+//     }
+//     this.items = data as ProductData[];
+//   } catch (err) {
+//     alert(err);
+//   }
+// }
+//
+// async updateCategories() {
+//   try {
+//     const controller = new AbortController();
+//     const { data, onError } = await fetcher(makeFetchUrl(), controller);
+//     if (onError?.[0] === true) {
+//       throw onError[1];
+//     }
+//     this.categories = data as ProductCategories;
+//   } catch (err) {
+//     console.log('123error');
+//     alert(err);
+//   }
+// }
+//
+// async loadNewItems(category: string) {
+//   try {
+//     const controller = new AbortController();
+//     const { data, onError } = await fetcher(
+//       makeFetchUrl(category, amount),
+//       controller,
+//     );
+//     if (onError?.[0]) {
+//       throw onError[1];
+//     }
+//     this.items = data as ProductData[];
+//   } catch (err) {
+//     alert(err);
+//   }
+// }
+async function rootLoader() {
+  try {
+    const store = new Store();
+    const controllerCategories = new AbortController();
+    const { data: dataCategories, onError: onErrorCategories } = await fetcher(
+      makeFetchUrl(),
+      controllerCategories,
+    );
+
+    if (onErrorCategories && onErrorCategories[0]) {
+      throw new Error(
+        `error while loading categories: ${onErrorCategories[1]}`,
+      );
+    }
+
+    const controllerItems = new AbortController();
+
+    const { data: items, onError: onErrorItems } = await fetcher(
+      makeFetchUrl('', 5),
+      controllerItems,
+    );
+
+    if (onErrorItems && onErrorItems[0]) {
+      throw new Error(`error while loading items: ${onErrorItems[1]}`);
+    }
+    store.addCategories(dataCategories as ProductCategories);
+    store.addItems(items as ProductData[]);
+    return { store };
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 const route = createBrowserRouter([
@@ -17,14 +95,13 @@ const route = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    loader: storeLoader,
+    loader: rootLoader,
     hydrateFallbackElement: <h1>cargandooooo</h1>,
 
     children: [
       {
         path: '/:categoryName',
         element: <CategoryPage />,
-        loader: storeLoader,
       },
       { path: '/item/:id', element: <ProductDetail /> },
     ],
--- src/tsx/routes/categoryPage.tsx
+++ src/tsx/routes/categoryPage.tsx
@@ -1,21 +1,35 @@
-import { useEffect, useState } from 'react';
-import { Navigate, useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 import { Store } from '../store';
+import { useEffect, useState } from 'react';
+import { ProductData } from '../types/global';
 
 export default function CategoryPage() {
   const { categoryName } = useParams();
   const { store } = useLoaderData<{ store: Store }>();
+  const [items, setItems] = useState<ProductData[]>([]);
+
+  useEffect(() => {
+    if (!categoryName) {
+      throw new Error('Cannot load the page');
+    }
+    store.loadNewItems(categoryName).then((bla) => {
+      console.log(bla);
+      if (!bla) return;
+      setItems(bla);
+      console.log(bla);
+    });
+  }, [categoryName, store]);
 
   if (!categoryName || !store) {
     throw new Error('Cannot load the page');
   }
-
   if (!store.allCategories.includes(categoryName)) {
     return <h1>La categoría {categoryName} no existe en nuestro catálogo</h1>;
   }
   return (
     <>
       <h1>{categoryName}</h1>
+      {items && items.map((item) => <div>{item.title}</div>)}
     </>
   );
 }

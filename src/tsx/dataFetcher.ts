510fef462c4640ddd4f354aa8bcb8b021fe9d3ac
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-12 11:31:06 -0500

    Fix data source for itemsArray and add 2 spaces indent

--- src/tsx/dataFetcher.ts
+++ src/tsx/dataFetcher.ts
@@ -1,74 +1,72 @@
 import {
-    FetchReturn,
-    FetchError,
-    DataFetched,
-    ErrorState,
-    ProductData,
-    ApiUrl,
-    CategoryPath,
-    ProductCategories,
+  FetchReturn,
+  FetchError,
+  DataFetched,
+  ErrorState,
+  ProductData,
+  ApiUrl,
+  CategoryPath,
+  ProductCategories,
 } from './types/global';
 
 const baseUrl = 'https://fakestoreapi.in/api/products' as const;
 
 const makeFetchUrl = (
-    category: string = '',
-    limit: number | undefined = undefined,
+  category: string = '',
+  limit: number | undefined = undefined,
 ): ApiUrl => {
-    const categoryName = category ? `/category?type=${category}` : '/category';
-    const amountLimit = limit ? `?limit=${limit}` : '';
-    return `${baseUrl}${limit && !category ? amountLimit : `${categoryName}${amountLimit}`}` as CategoryPath;
+  const categoryName = category ? `/category?type=${category}` : '/category';
+  const amountLimit = limit ? `?limit=${limit}` : '';
+  return `${baseUrl}${limit && !category ? amountLimit : `${categoryName}${amountLimit}`}` as CategoryPath;
 };
 
 const itemBuilder = (item: Record<string, string>): ProductData => ({
-    id: item.id,
-    title: item.title,
-    price: +item.price,
-    description: item.description,
-    image: item.image,
-    category: item.category,
-    brand: item.brand,
-    discount: +item.discount,
+  id: item.id,
+  title: item.title,
+  price: +item.price,
+  description: item.description,
+  image: item.image,
+  category: item.category,
+  brand: item.brand,
+  discount: +item.discount,
 });
 
 const fetcher = async (
-    url: ApiUrl,
-    controller: AbortController,
+  url: ApiUrl,
+  controller: AbortController,
 ): Promise<FetchReturn> => {
-    let loaded: boolean = false;
-    let onError: ErrorState = null;
-    let data: DataFetched = null;
+  let loaded: boolean = false;
+  let onError: ErrorState = null;
+  let data: DataFetched = null;
 
-    try {
-        const response = await fetch(url, {
-            mode: 'cors',
-            signal: controller.signal,
-        });
+  try {
+    const response = await fetch(url, {
+      mode: 'cors',
+      signal: controller.signal,
+    });
 
-        if (!response.ok) {
-            const { errors } = await response.json();
+    if (!response.ok) {
+      const { errors } = await response.json();
 
-            throw {
-                code: response.status,
-                description: errors,
-            } as FetchError;
-        }
-
-        const dataFetched = await response.json();
-        if (!dataFetched[0]?.id) {
-            data = dataFetched.categories as ProductCategories;
-        } else {
-            data = dataFetched.map(itemBuilder) as ProductData[];
-        }
-    } catch (err) {
-        const { code = 'unknown', description = 'unknown' } = err as FetchError;
-        const errPrompt = `Error ${code}: ${description}`;
-        onError = [true, errPrompt];
-    } finally {
-        loaded = true;
+      throw {
+        code: response.status,
+        description: errors,
+      } as FetchError;
     }
 
-    return { loaded, onError, data };
+    const dataFetched = await response.json();
+    data = dataFetched.categories
+      ? (dataFetched.categories as ProductCategories)
+      : (dataFetched.products.map(itemBuilder) as ProductData[]);
+  } catch (err) {
+    const { code = 'unknown', description = 'unknown' } = err as FetchError;
+    const errPrompt = `Error ${code}: ${description}`;
+    onError = [true, errPrompt];
+  } finally {
+    loaded = true;
+  }
+
+  return { loaded, onError, data };
 };
 
 export { fetcher, makeFetchUrl };

fada44c35626d5447c016d0f16e0855b3000c19e
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-14 16:24:51 -0500

    links the detailpages with the store

--- src/tsx/routes/productDetail.tsx
+++ src/tsx/routes/productDetail.tsx
@@ -1,7 +1,23 @@
+import { useOutletContext, useParams } from 'react-router-dom';
+import { Store } from '../store';
+import { useEffect, useState } from 'react';
+import { ProductData } from '../types/global';
+
 export default function ProductDetail() {
+  const { store } = useOutletContext<{ store: Store }>();
+  const { id } = useParams();
+  const [item, setItem] = useState<ProductData | undefined>(undefined);
+
+  useEffect(() => {
+    if (id !== undefined && store !== undefined) {
+      setItem(() => store.getItem(id));
+    }
+  }, [id, store]);
   return (
     <>
-      <h2>Product</h2>
+      <h2>{id}</h2>
+      <h2>{item?.title}</h2>
+      <p>{item?.description}</p>
     </>
   );
 }

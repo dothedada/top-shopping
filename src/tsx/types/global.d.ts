0b07c7d97e63ce95d3d29a01e9478c33a2859a1c
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-14 16:22:49 -0500

    Updates productData type

--- src/tsx/types/global.d.ts
+++ src/tsx/types/global.d.ts
@@ -1,12 +1,12 @@
 export interface ProductData {
-    id: string;
-    title: string;
-    price: number;
-    description: string;
-    brand: string;
-    image: string;
-    category: string;
-    discount: number;
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  brand: string;
+  image: string;
+  category: string;
+  discount: number;
 }
 
 export type ProductCategories = string[];
@@ -14,27 +14,27 @@ export type ProductCategories = string[];
 export type ErrorState = [true, string] | null;
 
 export interface FetchError {
-    code: number | 'unknown';
-    description: string[];
+  code: number | 'unknown';
+  description: string[];
 }
 
 export type FetchReturn = {
-    loaded: boolean;
-    data: DataFetched;
-    onError: ErrorState;
+  loaded: boolean;
+  data: DataFetched;
+  onError: ErrorState;
 };
 
 export type DataFetched = ProductData[] | ProductCategories | null;
 
 export type CategoryPath =
-    | '/category'
-    | `/category?type=${string}`
-    | `/category?type=${string}?limit=${number}`
-    | `?limit=${number}`;
+  | '/category'
+  | `/category?type=${string}`
+  | `/category?type=${string}?limit=${number}`
+  | `?limit=${number}`;
 
 export type ApiUrl = `${typeof baseUrl}${CategoryPath}`;
 
 type ErrorReactRouter = {
-    statusText: string;
-    message: string;
+  statusText: string;
+  message: string;
 };

626a107f65ed8f625c3946d9c8c7e1e0755b9b3b
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-20 12:21:10 -0500

    Adds and links styles.css

--- src/tsx/main.tsx
+++ src/tsx/main.tsx
@@ -1,3 +1,4 @@
+import '../css/styles.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';

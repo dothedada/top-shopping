c154693acdfd3cc1d1432807efea597e6e50cee4
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-24 12:37:59 -0500

    Adds items alike in the cart

--- src/tsx/routes/myCart.tsx
+++ src/tsx/routes/myCart.tsx
@@ -1,15 +1,35 @@
 import { useOutletContext } from 'react-router-dom';
 import { Cart } from '../cart';
 import { ItemList, EmptyCart } from '../components/itemsInCart';
+import { Store } from '../store';
+import { ItemCard } from '../components/itemsInDisplay';
+import { ProductData } from '../types/global';
 
 export default function MyCart() {
-  const { cart } = useOutletContext<{ cart: Cart }>();
+  const { store, cart, setItemsInCart } = useOutletContext<{
+    store: Store;
+    cart: Cart;
+    setItemsInCart: (amount: number) => void;
+  }>();
 
   if (cart === null) {
     return (
       <EmptyCart message="No se pudo cargar el carrito de compras, intentalo de nuevo más tarde" />
     );
   }
+
+  const presentCategories = cart.getItemsInCart.reduce<Set<string>>(
+    (present, current) => {
+      present.add(current.category);
+      return present;
+    },
+    new Set(),
+  );
+
+  const addBtn = (id: number): void => {
+    cart.addItem(id);
+    setItemsInCart(cart.totalItems);
+  };
 
   return (
     <>
@@ -29,6 +49,11 @@ export default function MyCart() {
           {cart.totalCost.savings}
         </p>
       )}
+      <h3>Tal vez te pueda interezar... </h3>
+      {store.similarItemsIds([...presentCategories], 5).map((itemId) => {
+        const item = store.getItem(itemId) as ProductData;
+        return <ItemCard key={itemId} addBtn={addBtn} item={item} />;
+      })}
     </>
   );
 }
